3ab6bd317f3bc4fa11fcc48bdd5548fcae8dd9a1
Andreas Wänqvist <andrkarl@gmail.com>
2017-02-21 12:29:59 +0100

    bot component start

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { autoinject } from 'aurelia-framework';
+import { RouterConfiguration, Router } from 'aurelia-router';
+
+@autoinject
 export class App {
-  message = 'Hello World!';
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    config.title = 'Title';
+    config.map([
+      { route: ['', 'bot'], name: 'bot', moduleId: 'views/bot-demo', title: 'bot demo' }
+    ]);
+  }
 }
---
+++ src/resources/elements/droid-bot.ts
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+
+export class DroidBot {
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+
+  }
+}
+
