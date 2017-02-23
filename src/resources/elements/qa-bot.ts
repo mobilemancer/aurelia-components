e84b4f2d6edf287af48dc87d15da394e99d62ee8
Andreas Wänqvist <andrkarl@gmail.com>
2017-02-23 21:24:04 +0100

    botbot!

--- src/resources/elements/qa-bot.ts
+++ src/resources/elements/qa-bot.ts
@@ -1,12 +1,61 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, autoinject } from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
 
+@autoinject
 export class QaBot {
   @bindable name;
   @bindable secret;
+
+  private question: string;
+  private expressions: Array<Expression> = [];
 
   private get url() {
     return 'https://webchat.botframework.com/embed/' + this.name + '\\?s=' + this.secret;
   }
 
+  constructor(private http: HttpClient) {
+    http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl(' https://westus.api.cognitive.microsoft.com/qnamaker/v1.0');
+    });
+  }
+
+
+  private keyPressed(event) {
+    if (event.keyCode === 13) {
+      this.send();
+    }
+    return true;
+  }
+
+  public send() {
+    var myHeaders = new Headers();
+    myHeaders.append("Ocp-Apim-Subscription-Key", "39931b6971294fa398c52c54889c4136");
+
+    var q = { "question": this.question };
+
+    const request: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      cache: "no-cache",
+      body: json(q)
+    };
+
+    this.expressions.push({ text: this.question, score: 100, type: "question" });
+
+    this.http.fetch("/knowledgebases/905f81b1-af8d-4276-8194-8bf0f22ef8b2/generateAnswer", request)
+      .then(response => response.json())
+      .then(data => {
+        this.expressions.push({ text: (<any>data).answer, score: (<any>data).score, type: "answer" });
+        this.question = "";
+        console.log(data);
+      });
+  }
 }
 
+interface Expression {
+  text: string;
+  score: number;
+  type: string;
+}
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/url-search-params-typing/index.d.ts" />
+/// <reference path="globals/whatwg-fetch/index.d.ts" />
+/// <reference path="globals/whatwg-streams/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
