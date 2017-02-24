c7e9a23d6ffaed6107eaf2f60f4cb1d0164f2f62
Andreas Wänqvist <andrkarl@gmail.com>
2017-02-24 21:05:40 +0100

    more

--- src/resources/elements/qa-bot.ts
+++ src/resources/elements/qa-bot.ts
@@ -1,26 +1,27 @@
 import { bindable, autoinject } from 'aurelia-framework';
 import { HttpClient, json } from 'aurelia-fetch-client';
+import { BindingEngine } from 'aurelia-binding';
+
+const SUBSCRIPTION_KEY = "39931b6971294fa398c52c54889c4136";
 
 @autoinject
 export class QaBot {
-  @bindable name;
-  @bindable secret;
-
   private question: string;
   private expressions: Array<Expression> = [];
+  private chatArea;
 
-  private get url() {
-    return 'https://webchat.botframework.com/embed/' + this.name + '\\?s=' + this.secret;
-  }
-
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private bindingEngine: BindingEngine) {
     http.configure(config => {
       config
         .useStandardConfiguration()
-        .withBaseUrl(' https://westus.api.cognitive.microsoft.com/qnamaker/v1.0');
+        .withBaseUrl('https://westus.api.cognitive.microsoft.com/qnamaker/v1.0');
     });
   }
 
+  attached() {
+    let subscription = this.bindingEngine.propertyObserver(this.chatArea, 'scrollHeight')
+      .subscribe((newValue, oldValue) => this.scrollChatArea());
+  }
 
   private keyPressed(event) {
     if (event.keyCode === 13) {
@@ -29,28 +30,38 @@ export class QaBot {
     return true;
   }
 
-  public send() {
-    var myHeaders = new Headers();
-    myHeaders.append("Ocp-Apim-Subscription-Key", "39931b6971294fa398c52c54889c4136");
+  private send() {
+    const headers = new Headers();
+    headers.append("Ocp-Apim-Subscription-Key", SUBSCRIPTION_KEY);
+    headers.append("Content-Type", "application/json");
 
-    var q = { "question": this.question };
+    const question = { "question": this.question };
 
     const request: RequestInit = {
       method: "POST",
-      headers: myHeaders,
+      headers: headers,
       cache: "no-cache",
-      body: json(q)
+      body: json(question)
     };
 
     this.expressions.push({ text: this.question, score: 100, type: "question" });
 
     this.http.fetch("/knowledgebases/905f81b1-af8d-4276-8194-8bf0f22ef8b2/generateAnswer", request)
       .then(response => response.json())
-      .then(data => {
-        this.expressions.push({ text: (<any>data).answer, score: (<any>data).score, type: "answer" });
-        this.question = "";
-        console.log(data);
+      .then((data: any) => {
+        this.expressions.push({ text: data.answer, score: data.score, type: "answer" });
+
+        this.clearQuestionField();
+        // this.scrollChatArea();
       });
+  }
+
+  private scrollChatArea() {
+    this.chatArea.scrollTop = this.chatArea.scrollHeight;
+  }
+
+  private clearQuestionField() {
+    this.question = "";
   }
 }
 
