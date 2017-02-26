ac2b7707b260ec82e0a6942dc79d4368af5ddd87
Andreas Wänqvist <andrkarl@gmail.com>
2017-02-26 02:04:31 +0100

    End of droid

--- src/resources/elements/qa-bot.ts
+++ src/resources/elements/qa-bot.ts
@@ -1,28 +1,33 @@
 import { bindable, autoinject } from 'aurelia-framework';
-import { HttpClient, json } from 'aurelia-fetch-client';
 import { BindingEngine } from 'aurelia-binding';
+import { HttpClient, json } from 'aurelia-fetch-client';
 
+const API_URL = "https://westus.api.cognitive.microsoft.com/qnamaker/v1.0";
 const SUBSCRIPTION_KEY = "39931b6971294fa398c52c54889c4136";
-const QNA_ADDRESS = "/knowledgebases/905f81b1-af8d-4276-8194-8bf0f22ef8b2/generateAnswer";
+const KB_ID = "905f81b1-af8d-4276-8194-8bf0f22ef8b2";
 const ENTER_KEY = 13;
 
 @autoinject
 export class QaBot {
   private question: string;
   private expressions: Array<Expression> = [];
-  private chatArea;
+  private chatArea: HTMLElement;
+  private subscription: any;
 
   constructor(private http: HttpClient, private bindingEngine: BindingEngine) {
     http.configure(config => {
       config
         .useStandardConfiguration()
-        .withBaseUrl('https://westus.api.cognitive.microsoft.com/qnamaker/v1.0');
+        .withBaseUrl(API_URL);
     });
   }
 
   attached() {
-    let subscription = this.bindingEngine.propertyObserver(this.chatArea, 'scrollHeight')
-      .subscribe((newValue, oldValue) => this.scrollChatArea());
+    this.subscription = this.bindingEngine.propertyObserver(this.chatArea, 'scrollHeight').subscribe((newValue, oldValue) => this.scrollChatArea());
+  }
+
+  detached() {
+    this.subscription.dispose();
   }
 
   private keyPressed(event) {
@@ -47,11 +52,10 @@ export class QaBot {
     };
 
     this.expressions.push({ text: this.question, score: 100, type: "question" });
-
-    this.http.fetch(QNA_ADDRESS, request)
+    this.http.fetch(`/knowledgebases/${KB_ID}/generateAnswer`, request)
       .then(response => response.json())
       .then((data: any) => {
-        this.expressions.push({ text: data.answer, score: data.score, type: "answer" });
+        this.expressions.push({ text: data.answer.replace("\n", "<br>"), score: data.score, type: "answer" });
         this.clearQuestionField();
       });
   }
